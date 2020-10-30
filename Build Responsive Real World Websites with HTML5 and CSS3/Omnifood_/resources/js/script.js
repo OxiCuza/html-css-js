a7f972dd0f705f03c9c7634544bf366c2b1ad4ad
Oxi_Cuza <oxicusa@gmail.com>
2020-10-30 19:39:56 +0700

    scrolling to elements

--- Build Responsive Real World Websites with HTML5 and CSS3/Omnifood_/resources/js/script.js
+++ Build Responsive Real World Websites with HTML5 and CSS3/Omnifood_/resources/js/script.js
@@ -8,4 +8,25 @@ $(document).ready(function() {
     }, {
         offset: '30%',
     });
+
+    $('.btn-to-plan--js').click(function() {
+        $('html, body').animate({scrollTop: $('.section-plans--js').offset().top}, 1000);
+    });
+
+    $('.btn-to-features--js').click(function() {
+        $('html, body').animate({scrollTop: $('.section-features--js').offset().top}, 700);
+    });
+
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+            return false;
+            }
+        }
+    });
 });
